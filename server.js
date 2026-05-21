663f4b8b99b23416e7b38a9b688482978e452c32
jashwanth sai <mjashwanth2007@gmail.com>
2026-05-21 16:32:35 +0530

    fixed trust proxy

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const enquiryRoutes     = require('./routes/enquiries');
 const adminRoutes       = require('./routes/admin');
 
 const app  = express();
-app.set('trust proxy', true);
+app.set('trust proxy', 1);
 const PORT = process.env.PORT || 3000;
 
 /* ── Security headers ── */
